35b21aab69dbdd136872dee5c00402ae2dd4857d
TSANI NUR SYA'BANI <sanivv757@gmail.com>
2023-12-22 00:03:51 +0700

    Add files via upload

--- main.php
+++ main.php
@@ -8,35 +8,41 @@ $data = query("SELECT * FROM data");
 
 
 <?php ob_start(); ?>
-<h1>Data</h1>
-
-<table border="1" cellpadding="10" cellspacing="0">
-
-    <tr>
-        <th>No</th>
-        <th>Nama</th>
-        <th>Email</th>
-        <th>Jurusan</th>
-        <th>Perintah</th>
-    </tr>
-
-    <?php $i = 1 ?>
-    <?php foreach ($data as $dat) : ?>
-        <tr>
-            <td><?= $i; ?></td>
-            <td><?= $dat["nama_mhs"]; ?></td>
-            <td><?= $dat["email"]; ?></td>
-            <td><?= $dat["jurusan"]; ?></td>
-            <td>
-                <a href="update.php">Ubah</a> ||
-                <a href="delete.php">Hapus</a>
-            </td>
-        </tr>
-        <?php $i++; ?>
-    <?php endforeach ?>
-</table>
-<br>
-<a href="create.php"><button>Tambah Data</button></a>
+<div class="container mt-5">
+    <div class="d-flex justify-content-center align-item-center" style="height: 10vh;">
+        <h1 class="text-center">Data-Data Mahasiswa</h1>
+    </div>
+    <table class="table table-bordered tabel-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+                <th>Perintah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $i = 1; ?>
+            <?php foreach ($data as $dat) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td><?= $dat["nama_mhs"]; ?></td>
+                    <td><?= $dat["email"]; ?></td>
+                    <td><?= $dat["jurusan"]; ?></td>
+                    <td>
+                        <a href="update.php?id=<?= $dat['id']; ?>" class="btn btn-sm btn-warning">Ubah</a>
+                        <a href="delete.php?id=<?= $dat['id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Apakah Anda yakin untuk menghapus data ini?')">Hapus</a>
+                    </td>
+                </tr>
+                <?php $i++; ?>
+            <?php endforeach; ?>    
+        </tbody>
+    </table>
+    <div class="d-flex justify-content-center">
+        <a href="create.php" class="btn btn-primary">Tambah Data</a>
+    </div>
+</div>
 <?php $content = ob_get_clean(); ?>
 
 
