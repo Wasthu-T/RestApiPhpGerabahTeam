bc1111d8e55f650a8c933f4cf886d8d676d4579b
TSANI NUR SYA'BANI <sanivv757@gmail.com>
2023-12-21 23:01:41 +0700

    Add files via upload

--- layout.php
+++ layout.php
@@ -5,20 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Document'; ?></title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <nav>
-        <h1>navbar</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand  fs-3"href="#">Gerabah</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <a class="nav-link fs-4" href="main.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link fs-4" href="create.php">Create</a>
+            </li>
+        </ul>
+        </div>
+    </div>
     </nav>
-
     <div id="content">
         <?= $content ?? ''; ?>
     </div>
-
-    <footer>
-        <p>&copy; 2023 Your Website</p>
+    <footer class="bg-dark text-white mt-5 p-3 text-center">
+        <p>&copy; 2023 Gerabah</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
+
+
